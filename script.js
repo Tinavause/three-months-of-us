cb0624e85d41d73f57588bf406f6867a9e9d74b0
Tina H. <53070485+Tinavause@users.noreply.github.com>
2026-05-26 14:10:01 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+let currentCard = 0;
+const cards = document.querySelectorAll(".card");
+
+function showCard(index) {
+  cards[currentCard].classList.remove("active");
+  currentCard = index;
+  cards[currentCard].classList.add("active");
+}
+
+function nextCard() {
+  if (currentCard < cards.length - 1) {
+    showCard(currentCard + 1);
+  }
+}
+
+function prevCard() {
+  if (currentCard > 0) {
+    showCard(currentCard - 1);
+  }
+}
+
+function showSurprise() {
+  document.getElementById("surprise").style.display = "block";
+  document.getElementById("restartBtn").style.display = "inline-block";
+}
+
+function restart() {
+  cards[currentCard].classList.remove("active");
+  currentCard = 0;
+  cards[currentCard].classList.add("active");
+
+  document.getElementById("surprise").style.display = "none";
+  document.getElementById("restartBtn").style.display = "none";
+}
+
+function startJourney() {
+  const music = document.getElementById("bgMusic");
+
+  if (music) {
+    music.volume = 0.25;
+    music.play();
+  }
+
+  nextCard();
+}
